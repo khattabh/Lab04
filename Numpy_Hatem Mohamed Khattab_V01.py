4588322ee0d40446328719e7ad3eb92c6102edab
khattabh <khattabhatem@gmail.com>
2026-01-03 14:37:48 +0200

    Initialized Lab4 Numpy task

---
+++ Numpy_Hatem Mohamed Khattab_V01.py
@@ -0,0 +1,58 @@
+# Mathematics I Lab 04 Numpy tasks
+# Hatem Mohamed Khattab
+
+import numpy as np
+import scipy as sp
+'''
+    Write a numpy program to create a structured array from
+    given student name, height, class and their data types.
+    Then sort the array on height.
+'''
+def student_array():
+    '''
+
+    '''
+    return 0
+
+'''
+    Using both Numpy and Scipy
+    Kindly solve the following equations using Scipy:
+    
+    ○ x + 2y - 3z + 2w = 30
+    ○ 2x - 5y + 4z + 9w = 4
+    ○ -5x + 40y - z - 20w = -6
+    ○ 5x - 4y - z + 60w = 5
+'''
+def solve_equations():
+    '''
+
+    '''
+    return 0
+
+'''
+    Implement the numpy vectorized version of the L2 loss function.
+'''
+def l2(yhat, y):
+    '''
+    Docstring for l2
+    
+    :param yhat: vector of size m (predicted labels)
+    :param y: vector of size m (true labels)
+    :return: value of the l2 loss function defined above
+    '''
+    
+    
+    loss = None
+    
+    return loss
+
+'''
+    Compare the performance of the dot product using parallelism in python
+    versus numpy_dot_product versus using a for loop and measure the time
+    needed for each.
+'''
+def compare_dot_product_performance():
+    '''
+
+    '''
+    return 0
